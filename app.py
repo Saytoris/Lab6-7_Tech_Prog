8af39c2091bc2ca1dd456e8992318d08ee726db7
Saytoris <igarlaptev@gmail.com>
2025-12-14 00:59:20 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+# app.py
+class SimpleCalculator:
+    """
+    Простий клас калькулятора для демонстрації CI/CD.
+    """
+    
+    def add(self, a, b):
+        """Повертає суму двох чисел."""
+        return a + b
+
+    def subtract(self, a, b):
+        """Повертає різницю двох чисел."""
+        return a - b
+    def subtrect(self, a, b):
+        """Повертає різницю двох чисел."""
+        return a - b
+
+if __name__ == "__main__":
+    print("Calculator app is running...")
