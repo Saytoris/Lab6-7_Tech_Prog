6ee50b156fc0e83ebfacda66b1b9bb6925e72550
Saytoris <igarlaptev@gmail.com>
2025-12-14 09:50:09 +0200

    Build once again

--- app.py
+++ app.py
@@ -160,3 +160,4 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     # host='0.0.0.0' ОБОВ'ЯЗКОВИЙ для того, щоб Docker міг "бачити" додаток
     app.run(debug=True, host='0.0.0.0', port=port)
+#1
